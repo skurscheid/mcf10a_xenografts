b266e4ea28b2cfc7996c0bc2df1ea22873d4388b
Sebastian Kurscheid <genomicsio@gmail.com>
2020-01-31 12:41:00 +1100

    adds missing SAM field in STAR output

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -2,4 +2,5 @@ import pandas as pd
 wildcards = dict()
 wildcards = {"batch" : "N1902403_RD_30-210828544_eukRNASEQ",
              "library" : "RNA-Seq",
-             "sample" : "KDD5-2_L4"}
+             "sample" : "KDD5-2_L4",
+             "ref_index" : "mmus_ensembl99"}
